a1f0714f94da10c855b8ced9f31ad4694bcd37aa
Zhansaule Telisheva <jansaule97@mail.ru>
2022-03-21 01:00:29 +0500

    services button opne modal window is added

--- src/modules/modalWindow.js
+++ src/modules/modalWindow.js
@@ -1,14 +1,25 @@
 const modalWindow = () => {
-  const callBackBtn = document.getElementsByClassName("callback-btn fancyboxModal hidden-xs")[0];
+  //const callBackBtn = document.getElementsByClassName("callback-btn fancyboxModal hidden-xs")[0];
+  //const servicesBtn = document.getElementsByClassName("button-services")[0];
+  const modalOpenBtns = document.querySelectorAll(".open-btn");
   const modal = document.querySelector(".modal-callback");
   const modalOverlay = document.querySelector(".modal-overlay");
   const closeBtns = document.querySelectorAll('.close-btn');
 
-  callBackBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.style.display = "block";
-    modalOverlay.style.display = "block";
+
+  modalOpenBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      modal.style.display = "block";
+      modalOverlay.style.display = "block";
+    });
   });
+
+  // callBackBtn.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   modal.style.display = "block";
+  //   modalOverlay.style.display = "block";
+  // });
 
   closeBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
