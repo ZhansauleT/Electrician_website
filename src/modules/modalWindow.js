d434c866415a033fddb453637fc575192080d61f
Zhansaule Telisheva <jansaule97@mail.ru>
2022-03-21 00:16:07 +0500

    modal close delegation is added

--- src/modules/modalWindow.js
+++ src/modules/modalWindow.js
@@ -2,31 +2,20 @@ const modalWindow = () => {
   const callBackBtn = document.getElementsByClassName("callback-btn fancyboxModal hidden-xs")[0];
   const modal = document.querySelector(".modal-callback");
   const modalOverlay = document.querySelector(".modal-overlay");
-  const closeBtn = document.querySelector('.modal-close');
-  const btn = document.querySelectorAll('a');
-
-  //console.log(closeBtn);
-
-
+  const closeBtns = document.querySelectorAll('.close-btn');
 
   callBackBtn.addEventListener('click', () => {
     modal.style.display = "block";
     modalOverlay.style.display = "block";
-    //console.log("opened");
   });
 
-  closeBtn.addEventListener('click', () => {
-    modal.style.display = "none";
-    modalOverlay.style.display = "none";
+  closeBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      modal.style.display = "none";
+      modalOverlay.style.display = "none";
+    });
   });
 
-  modalOverlay.addEventListener('click', () => {
-    modal.style.display = "none";
-    modalOverlay.style.display = "none";
-  });
-
-
-  console.log(btn);
 };
 
 export default modalWindow;
