a7a3d116348dbf9b34cc43070e2735f96fea6f18
Zhansaule Telisheva <jansaule97@mail.ru>
2022-03-20 22:25:50 +0500

    modal window module is added

--- src/modules/modalWindow.js
+++ src/modules/modalWindow.js
@@ -1,5 +1,32 @@
 const modalWindow = () => {
-  console.log("Modal Window Module");
+  const callBackBtn = document.getElementsByClassName("callback-btn fancyboxModal hidden-xs")[0];
+  const modal = document.querySelector(".modal-callback");
+  const modalOverlay = document.querySelector(".modal-overlay");
+  const closeBtn = document.querySelector('.modal-close');
+  const btn = document.querySelectorAll('a');
+
+  //console.log(closeBtn);
+
+
+
+  callBackBtn.addEventListener('click', () => {
+    modal.style.display = "block";
+    modalOverlay.style.display = "block";
+    //console.log("opened");
+  });
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = "none";
+    modalOverlay.style.display = "none";
+  });
+
+  modalOverlay.addEventListener('click', () => {
+    modal.style.display = "none";
+    modalOverlay.style.display = "none";
+  });
+
+
+  console.log(btn);
 };
 
 export default modalWindow;
