5fcc8fae5edd2eeebf48042f055649cf1f261d4e
Zhansaule Telisheva <jansaule97@mail.ru>
2022-03-22 19:03:01 +0500

    header links are fixed

--- src/modules/link.js
+++ src/modules/link.js
@@ -1,21 +1,33 @@
 const  link = () => {
   const logoLeft = document.querySelector(".logo.left>a");
-  const headerLinks = document.querySelector(".h-panel-right");
+  //const headerLinks = document.querySelector(".h-panel-right");
+  const headerEmail = document.querySelector(".header-email");
+  const headerTel = document.querySelector(".header-tel");
 
 
   logoLeft.href = "index.html";
 
-  headerLinks.childNodes.forEach((link) => {
-      link.innerHTML = `<a>${link.innerHTML}</a>`;
+  //header email link
+  headerEmail.innerHTML = `<a>${headerEmail.innerHTML}</a>`;
+  headerEmail.childNodes[0].href = "mailto:info@sitename.com";
+  headerEmail.cursor = "pointer";
 
-      if(link.childNodes[0] && link.childNodes[0].parentNode.className === 'header-email'){
-        link.childNodes[0].href = "mailto:info@sitename.com";
-      }else if(link.childNodes[0] && link.childNodes[0].parentNode.className === 'header-tel'){
-        link.childNodes[0].href = "tel:8(987)654-32-10";
-      }
+  //header phone number link
+  headerTel.innerHTML = `<a>${headerTel.innerHTML}</a>`;
+  headerTel.childNodes[0].href = "tel:8(987)654-32-10";
+  headerTel.cursor = "pointer";
+
+  // headerLinks.childNodes.forEach((link) => {
+  //     link.innerHTML = `<a>${link.innerHTML}</a>`;
+
+  //     if(link.childNodes[0] && link.childNodes[0].parentNode.className === 'header-email'){
+  //       link.childNodes[0].href = "mailto:info@sitename.com";
+  //     }else if(link.childNodes[0] && link.childNodes[0].parentNode.className === 'header-tel'){
+  //       link.childNodes[0].href = "tel:8(987)654-32-10";
+  //     }
       
-      link.cursor = "pointer";
-  });
+  //     link.cursor = "pointer";
+  // });
 
 };
 
